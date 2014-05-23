87b61cb7c10bffd6c55cd58763b2493850724a3c
Stephan Groen <stephan@logict.nl>
2014-05-23 12:53:01 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" version="-//W3C//DTD XHTML 1.1//EN" xml:lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="author" content="Stephan Groen" />
+
+    <title>Stephan Groen</title>
+    <style type="text/css" media="screen">
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            background-color: #eee;
+            font: 275% 'Georgia', sans-serif;
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            text-align: center;
+            margin-top: 75px;
+        }
+
+        h1 {
+            color: #333;
+            text-shadow: 0 1px 1px rgba(255, 255, 255, 0.75);
+        }
+
+        p.follow, p.company {
+            margin-top: 2em;
+            font-size: 62%;
+            text-shadow: 0 1px 1px rgba(255, 255, 255, 0.75);
+        }
+
+        p.company {
+            font-size: 40%
+        }
+
+        p.follow a, p.follow a:visited, p.company a, p.company a:visited {
+            font-style: italic;
+            color: #333;
+            text-decoration: underline;
+        }
+    </style>
+</head>
+
+<body>
+<h1>Stephan Groen</h1>
+<p class="follow">Volg mij op <a href="http://www.twitter.com/stephangroen">Twitter</a> of bezoek mijn profiel op <a href="http://www.linkedin.com/in/stephangroen">LinkedIn</a>.</p>
+<p class="company">Bekijk de website van <a href="http://www.logict.nl" style="color: #00B401">Logict</a>, makers van software zonder handleiding.</p><p class="company">Gemakkelijk, snel en digitaal factureren met <a href="http://www.earny.nl" style="color: #93117E">Earny, online factureren</a>.</p><p class="company"> <a href="http://www.picqer.com" style="color: #0078BE">Picqer, online voorraadbeheer</a>: Eindelijk echt eenvoudige software voor voorraadbeheer en orderpicking!</p>
+</body>
+</html>
